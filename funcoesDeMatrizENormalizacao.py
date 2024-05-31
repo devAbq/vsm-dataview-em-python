c5b1a1175c2449267f07b087f74300984f8ed186
Paulo Albuquerque <paulo.carvalho.albuquerque@gmail.com>
2024-05-31 12:18:29 -0300

    arquivo criado para externalizar as funcoes de tratamento de normalicacao e matrizes

---
+++ funcoesDeMatrizENormalizacao.py
@@ -0,0 +1,5 @@
+global funcaoNormalizadora
+
+def funcaoNormalizadora(vetorANormalizar):
+    equacaoDeNormalizacao = 2*vetorANormalizar-((vetorANormalizar.max()+vetorANormalizar.min()))/(vetorANormalizar.max()-vetorANormalizar.min())
+    return equacaoDeNormalizacao
