903fe71cc3fff70882b0cb8df8089e9644d733d9
Paulo Albuquerque <paulo.carvalho.albuquerque@gmail.com>
2024-06-06 17:21:22 -0300

    funcao de identificar metade do vetor externalizada

--- funcoesDeMatrizENormalizacao.py
+++ funcoesDeMatrizENormalizacao.py
@@ -1,5 +1,9 @@
-global funcaoNormalizadora
+global funcaoNormalizadora, metadeDoVetor
 
 def funcaoNormalizadora(vetorANormalizar):
     equacaoDeNormalizacao = (2*vetorANormalizar-((vetorANormalizar.max()+vetorANormalizar.min())))/(vetorANormalizar.max()-vetorANormalizar.min())
     return equacaoDeNormalizacao
+
+
+def metadeDoVetor(vetorAlvo):
+    return (len(vetorAlvo)//2)
