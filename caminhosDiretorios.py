845685bdf4f565c06264d759e4f87d708c57be78
Paulo Albuquerque <paulo.carvalho.albuquerque@gmail.com>
2024-06-12 08:55:20 -0300

    refatoracao do esquema de nomes

---
+++ caminhosDiretorios.py
@@ -0,0 +1,4 @@
+global dirDadosDesteDispositivo, dirGraficosDesteDispositivo
+
+dirDadosDesteDispositivo = "C:/Users/Albuquerque/Desktop/Scripts/analiseDeDadosVsmEmPython/diretorioDados/"
+dirGraficosDesteDispositivo = "C:/Users/Albuquerque/Desktop/Scripts/analiseDeDadosVsmEmPython/graficosGerados/"
